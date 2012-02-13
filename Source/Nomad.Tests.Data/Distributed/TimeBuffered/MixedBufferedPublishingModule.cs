8db9d8ad496b19d79d8523b61da09115190e81f3
Mikolaj Dobski <ankhazam@gmail.com>
2012-02-13 21:46:18 +0100

    added mixed publishing test for timelyBufferedPublish tests

---
+++ Source/Nomad.Tests.Data/Distributed/TimeBuffered/MixedBufferedPublishingModule.cs
@@ -0,0 +1,37 @@
+using System;
+using Nomad.Communication.EventAggregation;
+using Nomad.Messages.Loading;
+using Nomad.Tests.Data.Distributed.Commons;
+
+namespace Nomad.Tests.Data.Distributed.TimeBuffered
+{
+	/// <summary>
+	///		Sample class used for publishing data
+	/// </summary>
+	public class MixedBufferedPublishingModule : Nomad.Modules.IModuleBootstraper
+	{
+		private IEventAggregator _aggregator;
+		private IEventAggregatorTicket<NomadAllModulesLoadedMessage> _allModulesLoadedSubscriptionTicket;
+
+		public MixedBufferedPublishingModule(IEventAggregator eventAggregator)
+		{
+			_aggregator = eventAggregator;
+		}
+
+		public void OnLoad()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				string payload = "Sample Message " + i;
+				DistributableMessage message = new DistributableMessage(payload);
+				_aggregator.PublishTimelyBuffered(message, DateTime.Now+ new TimeSpan(0,0,1,0));
+				_aggregator.PublishTimelyBuffered(message, DateTime.MinValue);
+			}
+		}
+
+		public void OnUnLoad()
+		{
+			// do nothing
+		}
+	}
+}
