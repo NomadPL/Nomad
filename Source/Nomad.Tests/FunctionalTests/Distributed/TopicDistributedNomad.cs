5d7942aebda507ddbdca6ce155575a0cc5c8c16a
Mikolaj Dobski <ankhazam@gmail.com>
2012-02-13 13:08:51 +0100

    two listeners on distributed kernels work.

--- Source/Nomad.Tests/FunctionalTests/Distributed/TopicDistributedNomad.cs
+++ Source/Nomad.Tests/FunctionalTests/Distributed/TopicDistributedNomad.cs
@@ -21,12 +21,11 @@ namespace Nomad.Tests.FunctionalTests.Distributed
 		[Test]
 		public void module_publishes_module_listens()
 		{
-
 			// path for this test (using the test method name) use in each code
 			PrepareSharedLibrary();
 
-			string publishingModuleSrc = GetSourceCodePath(typeof(SimplePublishingModule));
-			string listeningModuleSrc = GetSourceCodePath(typeof(SimpleListeningModule));
+			string publishingModuleSrc = GetSourceCodePath(typeof (SimplePublishingModule));
+			string listeningModuleSrc = GetSourceCodePath(typeof (SimpleListeningModule));
 
 			string listener1 = GenerateListener(_runtimePath, _sharedDll, listeningModuleSrc, 1);
 
@@ -60,7 +59,7 @@ namespace Nomad.Tests.FunctionalTests.Distributed
 
 
 			// assert the events being published	
-			var fi = new FileInfo(listener1 +"_CounterFile");
+			var fi = new FileInfo(listener1 + "_CounterFile");
 			if (fi.Exists)
 			{
 				StreamReader counterReader = fi.OpenText();
@@ -76,10 +75,85 @@ namespace Nomad.Tests.FunctionalTests.Distributed
 		}
 
 		[Test]
-		[Ignore("Not yet implemented")]
 		public void one_module_publishes_two_module_listens()
 		{
-			
+			// path for this test (using the test method name) use in each code
+			PrepareSharedLibrary();
+
+			string publishingModuleSrc = GetSourceCodePath(typeof(SimplePublishingModule));
+			string listeningModuleSrc = GetSourceCodePath(typeof(SimpleListeningModule));
+
+			string listener1 = GenerateListener(_runtimePath, _sharedDll, listeningModuleSrc, 1);
+			string listener2 = GenerateListener(_runtimePath, _sharedDll, listeningModuleSrc, 2);
+
+			string publisherDll = Compiler.GenerateModuleFromCode(publishingModuleSrc, _sharedDll);
+			ManifestBuilderConfiguration manifestConfiguration = ManifestBuilderConfiguration.Default;
+			manifestConfiguration.ModulesDependenciesProvider = new SingleModulesDependencyProvider();
+			Compiler.GenerateManifestForModule(publisherDll, KeyFile, manifestConfiguration);
+
+			// create listener site
+			string listener1Site = "net.tcp://127.0.0.1:5555/IDEA";
+			string listener2Site = "net.tcp://127.0.0.1:6666/IDEA";
+
+			// create published sites
+			string publisherSite = "net.tcp://127.0.0.1:7777/IDEA";
+
+			// create listener kernels with configuration
+			NomadConfiguration config1 = NomadConfiguration.Default;
+			config1.DistributedConfiguration = DistributedConfiguration.Default;
+			config1.DistributedConfiguration.LocalURI = new Uri(listener1Site);
+			ListenerKernel = new NomadKernel(config1);
+			IModuleDiscovery listenerDiscovery = new SingleModuleDiscovery(listener1);
+			ListenerKernel.LoadModules(listenerDiscovery);
+
+			NomadConfiguration config2 = NomadConfiguration.Default;
+			config2.DistributedConfiguration = DistributedConfiguration.Default;
+			config2.DistributedConfiguration.LocalURI = new Uri(listener2Site);
+			ListenerKernel = new NomadKernel(config2);
+			IModuleDiscovery listener2Discovery = new SingleModuleDiscovery(listener2);
+			ListenerKernel.LoadModules(listener2Discovery);
+
+			// create publishing kernel
+			NomadConfiguration publisherConfig = NomadConfiguration.Default;
+			publisherConfig.DistributedConfiguration = DistributedConfiguration.Default;
+			publisherConfig.DistributedConfiguration.LocalURI = new Uri(publisherSite);
+			publisherConfig.DistributedConfiguration.URLs.Add(listener1Site);
+			publisherConfig.DistributedConfiguration.URLs.Add(listener2Site);
+			PublisherKernel = new NomadKernel(publisherConfig);
+			IModuleDiscovery publisherDiscovery = new SingleModuleDiscovery(publisherDll);
+			PublisherKernel.LoadModules(publisherDiscovery);
+
+
+			// assert the events being published	
+			// listener1
+			var fi = new FileInfo(listener1 + "_CounterFile");
+			if (fi.Exists)
+			{
+				StreamReader counterReader = fi.OpenText();
+				int value = Convert.ToInt32(counterReader.ReadLine());
+				// Verifying that locally the event aggregator works properly
+				Assert.AreEqual(5, value);
+				counterReader.Close();
+			}
+			else
+			{
+				Assert.Fail("No counter file from listener module in distributed configuration");
+			}
+
+			// listener2
+			fi = new FileInfo(listener2 + "_CounterFile");
+			if (fi.Exists)
+			{
+				StreamReader counterReader = fi.OpenText();
+				int value = Convert.ToInt32(counterReader.ReadLine());
+				// Verifying that locally the event aggregator works properly
+				Assert.AreEqual(5, value);
+				counterReader.Close();
+			}
+			else
+			{
+				Assert.Fail("No counter file from listener module in distributed configuration");
+			}	
 		}
 	}
 }
